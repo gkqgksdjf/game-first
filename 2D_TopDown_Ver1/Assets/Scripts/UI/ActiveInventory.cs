f0f7bc0771cd0eecaaa0f5d1be64335aa69a97fe
gkqgksdjf <72592919+gkqgksdjf@users.noreply.github.com>
2025-11-01 17:55:30 +0900

    25. Instantiating Weapons

--- 2D_TopDown_Ver1/Assets/Scripts/UI/ActiveInventory.cs
+++ 2D_TopDown_Ver1/Assets/Scripts/UI/ActiveInventory.cs
@@ -14,6 +14,8 @@ public class ActiveInventory : MonoBehaviour
 
     private void Start() {
         playerControls.Inventory.Keyboard.performed += ctx => ToggleActiveSlot((int)ctx.ReadValue<float>());
+
+        ToggleActiveHighlight(0);
     }
 
     private void OnEnable() {
@@ -37,6 +39,22 @@ public class ActiveInventory : MonoBehaviour
     }
 
     private void ChangeActiveWeapon() {
-        Debug.Log(transform.GetChild(activeSlotIndexNum).GetComponent<InventorySlot>().GetWeaponInfo().weaponPrefab.name);
+        if (ActiveWeapon.Instance.CurrentActiveWeapon != null) {
+            Destroy(ActiveWeapon.Instance.CurrentActiveWeapon.gameObject);
+        }
+
+        if (!transform.GetChild(activeSlotIndexNum).GetComponentInChildren<InventorySlot>()) {
+            ActiveWeapon.Instance.WeaponNull();
+            return;
+        }
+
+        GameObject weaponToSpawn = transform.GetChild(activeSlotIndexNum).
+        GetComponentInChildren<InventorySlot>().GetWeaponInfo().weaponPrefab;
+
+        GameObject newWeapon = Instantiate(weaponToSpawn, ActiveWeapon.Instance.transform.position, Quaternion.identity);
+
+        newWeapon.transform.parent = ActiveWeapon.Instance.transform;
+
+        ActiveWeapon.Instance.NewWeapon(newWeapon.GetComponent<MonoBehaviour>());
     }
 }
