3d97b08614610c12fc54d66d92c3af7d22ebf9f3
gkqgksdjf <72592919+gkqgksdjf@users.noreply.github.com>
2025-11-01 17:08:15 +0900

    23. IWeapon Interface
    Bow/Staff Log

---
+++ 2D_TopDown_Ver1/Assets/Scripts/Staff.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Staff : MonoBehaviour, IWeapon {
+    public void Attack() {
+        Debug.Log("Staff Attack");
+        ActiveWeapon.Instance.ToggleIsAttacking(false);
+    }
+}
